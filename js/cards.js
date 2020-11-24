91863487e2eb7fad3d240f5d0d5fc10cfe03274e
Ray Robertson <ray760@yahoo.com>
2020-11-24 07:14:07 -0500

    Working cards using json-server

--- js/cards.js
+++ js/cards.js
@@ -14,18 +14,18 @@ const cardData = async () => {
   cards.forEach( card => {
     cardTemplate += `
     <div class="usa-card desktop:grid-col-3 tablet:grid-col-4 mobile-lg:grid-col-6 grid-col-12">
-      <div class="usa-card__container">
+      <div class="usa-card__container" style="height: 280px; overflow-y: hidden">
         <header class="usa-card_header">
           <h5 class="usa-card__heading">${card.title}</h5>
         </head>
 
-        <div class="usa-card__body">
+        <div class="usa-card__body" style="padding: 0 !important">
           <p>${card.data}</p>
         </div>
         
-        <div class="usa-card__footer">
-          <button class="usa-button"><i class="fas fa-exclamation-circle"></i>INFO</button>
-          <button class=""><i class="fas fa-external-link-alt"></i>GO TO APP</button>
+        <div class="usa-card__footer" style="justify-content: space-between; display: flex; padding-left: 0; padding-right: 0">
+          <button class="usa-button usa-button--outline" style="height: 40px"><i class="fas fa-exclamation-circle"></i>INFO</button>
+          <button class="usa-button" style="height: 40px"><i class="fas fa-external-link-alt"></i>GO TO APP</button>
         </div>
       </div>
     </div>
